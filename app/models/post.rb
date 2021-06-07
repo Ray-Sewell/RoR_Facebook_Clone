171be000cfdba5626e5917fc5e353e0f35a4b8a0
Ray <sewellray11+git@googlemail.com>
2021-06-07 16:26:45 +0100

    cleaned up feed and user views

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: "User", foreign_key: "author_id"
     has_many :comments
 
-    has_many :groups_posts
-    has_many :groups, through: :groups_posts, source: :group
+    has_one :groups_post
+    has_one :group, through: :groups_post, source: :group
 
     attr_accessor :group_id
 
     has_many :likes
+    has_many :user_likes, through: :likes, source: :user
 end
